b21e0dbd4b754590626f1a7243ee03359bbe9569
QuickPrimeTech <quickprimetech@gmail.com>
2025-07-14 09:38:26 +0300

    reached a decent point for the gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import { GalleryFiltersPage } from "@/sections/gallery/gallery-filters";
+import GalleryHeader from "@/sections/gallery/hero";
+
+export default function Gallery() {
+  return (
+    <>
+      <GalleryHeader />;
+      <GalleryFiltersPage />
+    </>
+  );
+}
